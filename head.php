f06bbbabbadbf4e5db2a36154ae561deec81d259
Léo <leo.rodriguessc@gmail.com>
2022-11-26 22:39:07 -0300

    Fix HTML Structure

---
+++ head.php
@@ -0,0 +1,18 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BuySkinLoL</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        clifford: '#da373d',
+                    }
+                }
+            }
+        }
+    </script>
+</head>
--- index.php
+++ index.php
@@ -1,2 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<?php
+require_once "head.php";
+?>
+<body>
 
-<?php require_once "site/home.php"?>
+<?php
+require_once "site/home.php";
+
+?>
+
+</body>
+</html>
